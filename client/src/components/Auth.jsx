754541172ffb083dc4b7aa3ec49e1d2bc94b68f1
Sumit Prajapati <sumitp0808@gmail.com>
2025-06-12 19:56:01 +0530

    Update Auth.jsx
    linked deployed backend

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io("https://thinkboard-backend-k7ec.onrender.com", {
   withCredentials: true,
   transports: ["websocket"],
 });
